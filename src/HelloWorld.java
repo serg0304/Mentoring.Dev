d1def324be08374b7e51fa9efefeae016258c4c3
serg0304 <warbdq@gmail.com>
2026-05-05 22:39:49 -0400

    Test 5/5/2026

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 20;
+        String message = "Hi";
+        System.out.println(message + " " + (a  + b));
+        System.out.println(a + b + message);
+
+        String name = "Milan";
+        System.out.println(name.length());
+        System.out.println(name.length());
+        System.out.println(name.length());
+        System.out.println(name.length());
+
+    }
+}
